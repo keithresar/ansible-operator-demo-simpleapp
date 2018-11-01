a90aba351b82128c59e7a721269c4b5f8130dfc9
Keith Resar <keithresar@users.noreply.github.com>
2018-11-01 16:47:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,39 +2,43 @@
 <?php
 
 
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("DATABASE_USER");
-$dbpwd = getenv("DATABASE_PASSWORD");
-$dbname = getenv("DATABASE_NAME");
 
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno)  {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-// DDL blind creation
-$rs = $connection->query("create table version  ( cur_version int not null, primary key (cur_version))");
 
 echo "Welcome to my stateless web frontend.<br>\n";
 
 
 
+if (getenv("MYSQL_SERVICE_HOST"))  {
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("DATABASE_USER");
+	$dbpwd = getenv("DATABASE_PASSWORD");
+	$dbname = getenv("DATABASE_NAME");
 
-// Get current version
-$rs = $connection->query("select cur_version from version");
-if (!mysqli_num_rows($rs))  $row['cur_version'] = 0;
-else  $row = $rs->fetch_array(MYSQLI_ASSOC);
-print "Currently executing stateful data <span style='display:inline-block;background-color:yellow;padding:.2em 1em;'>version ".$row['cur_version']."</span><br>\n";
+	$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	if ($connection->connect_errno)  {
+	    printf("Connect failed: %s\n", $mysqli->connect_error);
+	    exit();
+	}// DDL blind creation
+	$rs = $connection->query("create table version  ( cur_version int not null, primary key (cur_version))");
+
+
+	// Get current version
+	$rs = $connection->query("select cur_version from version");
+	if (!mysqli_num_rows($rs))  $row['cur_version'] = 0;
+	else  $row = $rs->fetch_array(MYSQLI_ASSOC);
+	print "Currently executing stateful data <span style='display:inline-block;background-color:yellow;padding:.2em 1em;'>version ".$row['cur_version']."</span><br>\n";
+
+	$rs->close();
+	$connection->close();
+}  else  {
+	print "No stateful data available<br>\n";
+}
 
 
 echo "<hr>\n";
 echo "Serving from pod <b>".getenv('HOSTNAME')."</b>.\n";
 
-
-$rs->close();
-$connection->close();
 
 ?>
 </body>
