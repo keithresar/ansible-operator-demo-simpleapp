f62ae735b83ede2325e09c0151dd70a3d8a1f738
Keith Resar <keithresar@users.noreply.github.com>
2018-11-05 16:41:42 -0600

    Create api.php

---
+++ api.php
@@ -0,0 +1,34 @@
+<?php
+
+
+if (gethostbyname("mariadb")!="mariadb")  {
+        $dbhost = "mariadb";
+        $dbuser = "root";
+        $dbpwd = "mypassword";
+        $dbname = "mydatabase";
+
+    $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+    if ($connection->connect_errno)  {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+    // DDL blind creation
+    $rs = $connection->query("create table if not exists version  ( cur_version int not null, primary key (cur_version))");
+
+    // Get current version
+    $rs = $connection->query("select MAX(cur_version) as cur_version from version");
+    if (!mysqli_num_rows($rs))  $row['cur_version'] = 0;
+    else  $row = $rs->fetch_array(MYSQLI_ASSOC);
+    $version = $row['cur_version'];
+
+    $rs->close();
+    $connection->close();
+}  else  {
+    $version = 0;
+}
+
+
+print json_encode(["version" => $version]);
+
+
+?>
